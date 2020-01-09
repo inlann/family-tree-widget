b0803528dd7a3c0240b8adc72318a080287011e8
Vivien Jovet <vivien.jovet@toro.io>
2020-01-09 15:22:01 +0800

    Comment out unused

--- test-widget/src/browser/test-widget-frontend-module.ts
+++ test-widget/src/browser/test-widget-frontend-module.ts
@@ -1,15 +1,14 @@
 import { bindViewContribution, FrontendApplicationContribution, WidgetFactory } from "@theia/core/lib/browser";
 import { ContainerModule } from "inversify";
 import "../../src/browser/style/index.css";
-import "../../src/browser/welcome/welcome.css";
+// import "../../src/browser/welcome/welcome.css";
 import { TestWidgetContribution } from "./test-widget-contribution";
 import { TestWidgetWidget } from "./test-widget-widget";
 import { createFamilyTreeWidget, FamilyTreeWidget } from "./tree";
 import { FamilyTreeWidgetContribution } from "./tree/family-tree-widget-contribution";
-import { WelcomeDataProvider, WelcomeDataProviderImpl } from "./welcome/welcome-data-provider";
-import { WelcomeViewContribution } from "./welcome/welcome-view-contribution";
-import { WelcomeWidget } from "./welcome/welcome-widget";
-
+// import { WelcomeDataProvider, WelcomeDataProviderImpl } from "./welcome/welcome-data-provider";
+// import { WelcomeViewContribution } from "./welcome/welcome-view-contribution";
+// import { WelcomeWidget } from "./welcome/welcome-widget";
 
 export default new ContainerModule(bind => {
   bindViewContribution(bind, TestWidgetContribution);
@@ -22,13 +21,13 @@ export default new ContainerModule(bind => {
     }))
     .inSingletonScope();
 
-  bindViewContribution(bind, WelcomeViewContribution);
-  bind(WelcomeWidget).toSelf();
-  bind(WidgetFactory).toDynamicValue(context => ({
-    id: WelcomeWidget.ID,
-    createWidget: () => context.container.get<WelcomeWidget>(WelcomeWidget)
-  }));
-  bind(WelcomeDataProvider).to(WelcomeDataProviderImpl).inSingletonScope;
+  // bindViewContribution(bind, WelcomeViewContribution);
+  // bind(WelcomeWidget).toSelf();
+  // bind(WidgetFactory).toDynamicValue(context => ({
+  //   id: WelcomeWidget.ID,
+  //   createWidget: () => context.container.get<WelcomeWidget>(WelcomeWidget)
+  // }));
+  // bind(WelcomeDataProvider).to(WelcomeDataProviderImpl).inSingletonScope;
 
   // bind(LabelProviderContribution).to(TestLabelContribution);
 
